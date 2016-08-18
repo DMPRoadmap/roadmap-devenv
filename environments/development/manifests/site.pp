6892429ddbe9ebf06c4fcb6cdaaa030af931723f
Jimmy Angelakos <jimmy.angelakos@ed.ac.uk>
2016-08-18 13:18:07 +0100

    configured shared folder src/ for source code (visible in container /opt/src)

--- environments/development/manifests/site.pp
+++ environments/development/manifests/site.pp
@@ -2,15 +2,10 @@ node 'dmponline-dev' {
 
   include dcc::standard
   include dcc::epel
-  include dcc::rubyrails
   include dcc::gitclone
+  include dcc::rubyrails
   include dcc::webserver
   include dcc::updatedb
-
-#  include dcc::sshmotd
-#  include dcc::maven
-#  include dcc::copydb
-#  include dcc::firewall
 
 }
 
--- environments/development/modules/dcc/manifests/gitclone.pp
+++ environments/development/modules/dcc/manifests/gitclone.pp
@@ -5,25 +5,16 @@ class dcc::gitclone {
     ensure => installed,
   }
 
-  # Define the standard location for our source code
-  file { '/opt/src' :
-    ensure => directory,
-    group  => source,
-    owner  => vagrant,
-    mode   => '0755',
-  }
-
   # Define our main repository and bring in the revision we need from it
-  vcsrepo { '/opt/src/dmponline':
+  vcsrepo { '/opt/src/dmponline.git':
     ensure   => latest,
     owner    => vagrant,
     group    => source,
     provider => git,
     user     => vagrant,
-    require  => [Package['git'],
-                 File['/opt/src'], ],
+    require  => Package['git'],
     source   => 'https://github.com/DigitalCurationCentre/roadmap.git',
-    revision => 'development',
+    revision => 'master',
   }
 
 }
